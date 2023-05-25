508f198ee21e863c7db9779a0bb1a52073d72612
Khai Nguyen <nguyentrongkhaiptit@gmail.com>
2023-05-25 16:14:59 +0700

    add tc pricing

---
+++ Object Repository/Page_PhotoX/div_Join Us.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Join Us</name>
+   <tag></tag>
+   <elementGuidId>4471e532-bd98-4e33-ae52-87f30e861258</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='pricing']/div/div/div/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#pricing > div.container-fluid > div.row.text-muted.text-center > div.col.m-sm-4 > div.h1.display-4.mb-4</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>23af1c1b-c8c0-494c-9fde-8a80d7e0e7b8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>h1 display-4 mb-4</value>
+      <webElementGuid>e8b22801-da7b-4c89-b41a-fadb86c33c11</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Join Us</value>
+      <webElementGuid>8662d69d-7d7f-4a89-a504-2e559c3c72d3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;pricing&quot;)/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;row text-muted  text-center&quot;]/div[@class=&quot;col m-sm-4&quot;]/div[@class=&quot;h1 display-4 mb-4&quot;]</value>
+      <webElementGuid>1b470da3-c01e-4fee-98e7-cfdb128ed2ae</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='pricing']/div/div/div/div</value>
+      <webElementGuid>4656571b-f4c2-4079-ae4c-c201b2064812</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Helen'])[1]/following::div[4]</value>
+      <webElementGuid>5f432289-d2ab-4b4a-94a1-42641344e0a0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Johnathan'])[1]/following::div[5]</value>
+      <webElementGuid>e3c082d3-682f-4c74-a72c-452c26d2dd49</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Monthly Membership'])[1]/preceding::div[2]</value>
+      <webElementGuid>7e7491c0-ef5b-47ab-b25c-1fb8c7fba90e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Photoshop'])[1]/preceding::div[2]</value>
+      <webElementGuid>f60bdcfb-53b7-4879-945a-8f70b93e1763</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Join Us']/parent::*</value>
+      <webElementGuid>8cb4f34b-4970-4200-8580-5ed2ecc34f34</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section[3]/div/div/div/div</value>
+      <webElementGuid>8e231a8a-e6c4-43ce-b49f-104e85bfd75f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Join Us' or . = 'Join Us')]</value>
+      <webElementGuid>932070e5-57a2-4a69-be3c-9873debac0e5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
